0e5c6db3c5a2fa076782f19fcba75e6fb60582ef
cbeisel <collin.beisel@toyota.com>
2022-01-16 18:44:29 -0600

    added code comments and completed README

--- README.md
+++ README.md
@@ -1,5 +1,62 @@
 # Employee Vision
 
+ ![Github licence](http://img.shields.io/badge/license-MIT-blue.svg)
+
+ ## Table of Contents
+  * [Links](#links)
+  * [Description](#app-descriptino)
+  * [Technologies Used](#technologies-used)
+  * [Why I Built](#why-did-i-build-this-project)
+  * [What I learned](#what-did-i-learn)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Tests](#tests)
+  * [User Story](#User-Story)
+  * [Acceptance Criteria](#acceptance-criteria)
+
+![Main Menu Screenshot](https://github.com/collin-beisel-tm/employee-vision/blob/main/images/main-menu-screenshot.PNG)
+
+## Links
+ - Github Repo: https://github.com/collin-beisel-tm/employee-vision
+ - Video Demo: https://drive.google.com/file/d/1JgJFIjzG1xw6OYAR2dVB2eqA3J-RU496/view?usp=sharing
+## App Description
+
+Employee Vision is a Node.js console application that allows it's users to interface with a MYSQL database that holds employee data. Small companies can use this application to keep track of their Employees, departments, and roles. This app was built as an assignment for SMU's Full Stack Web Development Bootcamp.
+
+## Technologies used
+- JavaScript
+- Node.js
+- Express.js
+- MySQL2
+- Inquirer.js
+- Git/GitHub
+
+## Why did I build this project?
+I built this project as a bootcamp assignment. 
+
+## What did I learn?
+This project taught me how to use MySQL to create a database, tables, and modify them using a user friendly command line interface.
+
+## Installation
+ - Navigate to https://github.com/collin-beisel-tm/employee-vision
+ - click the download/clone the package
+ - Open with your favorite code editor
+ - Run NPM i
+ - update your mysql credentials in the connection section of the code
+ - SOURCE your schema and seeds files
+ - run npm start in your terminal
+ - The app will start 
+## license
+MIT License
+
+Copyright (c) [2021] [CollinBeisel]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ## User Story
 AS A business owner
 I WANT to be able to view and manage the departments, roles, and employees in my company
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Add near the top of the file
+// Require mysql, inquirer, console.table and express frameworks
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
 require("console.table");
@@ -16,21 +16,23 @@ const db = mysql.createConnection(
       password: 'test',
       database: 'employee_vision'
     },
+    //Let user know they are connected to the db.
     console.log('Using the Employee Vision database.')
   );
 
-  // Start server after DB db
+  // Start server after DB 
 db.connect(err => {
   if (err) throw err;
   console.log('Welcome User!');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-  
+  //kick off inquirer prompts
   initialPrompt();
  
 });
 
+//Function to display initial choices to user
 function initialPrompt() {
   inquirer
   .prompt({
@@ -50,6 +52,7 @@ function initialPrompt() {
   })
   .then(function(choices) {
 
+    //if user selects said option, run the associated function
     switch (choices.option) {
 
       case "View departments":
@@ -80,37 +83,46 @@ function initialPrompt() {
 }
 
 
-//Functions
+//Functions/SQL queries
 
+//query db to view all departments
 function viewDepartment() {
+  //grab all columns/rows from departments table and return it to console
   db.query("SELECT * FROM departments", function(err, res) {
     if (err) throw err;
     console.table(res);
+    //return to main menu
     initialPrompt();
   });
   }
   
+  //query db to view all roles
   function viewRoles() {
+    //select title, id, salary from roles, select department name from department and return them in a single table
     var query ="SELECT roles.title, roles.id, departments.dept_name, roles.salary FROM roles LEFT JOIN departments ON roles.dept_id = departments.id";
   db.query(query, function(err, res) {
     if (err) throw err;
     console.table(res);
+    //return to main menu
     initialPrompt();
   });
   }
   
+  // query db to view all employees
   function viewEmployees() {
+    //select all relevant fields from all 3 tables and join them into 1 table to return to user
     var query = "SELECT e.id, e.first_name, e.last_name, r.title, d.dept_name, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employees e LEFT JOIN roles r ON e.roles_id = r.id LEFT JOIN departments d ON d.id  = r.dept_id LEFT JOIN employees m ON m.id = e.manager_id";
   db.query(query, function(err, res) {
     if (err) throw err;
     console.table(res);
+    //return to main menu
     initialPrompt();
   });
  }
 
+ // Insert query to add a department
 function addDepartment() {
-
-
+  //get data from user
   inquirer.prompt({
     
       type: "input",
@@ -118,19 +130,19 @@ function addDepartment() {
       name: "dept"
 
   }).then(function(answer){
-
-
-
+    //insert user provided data into the appropriate fields
       db.query("INSERT INTO departments (dept_name) VALUES (?)", [answer.dept] , function(err, res) {
           if (err) throw err;
           console.table(res)
+          //return to main menu
           initialPrompt()
   })
   })
 };
 
-
+//insert query to add role
 function addRole() {
+  //gather data from user
 inquirer
   .prompt([
     {
@@ -150,9 +162,8 @@ inquirer
     }
   ])
   .then(function(answer) {
-
-
-    db.query("INSERT INTO roles (title, salary, departments_id) VALUES (?, ?, ?)", [answer.role, answer.salary, answer.deptID], function(err, res) {
+    // insert user provided data into the appropriate columns
+    db.query("INSERT INTO roles (title, salary, dept_id) VALUES (?, ?, ?)", [answer.role, answer.salary, answer.deptID], function(err, res) {
       if (err) throw err;
       console.table(res);
       initialPrompt();
@@ -160,8 +171,10 @@ inquirer
   });
 }
 
+// query to insert a new employee record
 function addEmployee() {
 inquirer
+//gather user data
   .prompt([
     {
       type: "input",
@@ -185,19 +198,20 @@ inquirer
     }
   ])
   .then(function(answer) {
-
-    
+    // insert all user provided data into the appropriate columns
     db.query("INSERT INTO employees (first_name, last_name, roles_id, manager_id) VALUES (?, ?, ?, ?)", [answer.firstName, answer.lastName, answer.rolesID, answer.managerID], function(err, res) {
       if (err) throw err;
       console.table(res);
+      //return to main menu
       initialPrompt();
     });
   });
 }
 
-
+//query to change an employees job title/role
 function updateEmployeesRole() {
 inquirer
+//get user data
   .prompt([
     {
       type: "input",
@@ -212,15 +226,19 @@ inquirer
     }
   ])
   .then(function(answer) {
+    //update existing datapoints for role id
     db.query('UPDATE employees SET roles_id=? WHERE id= ?',[answer.updateRole, answer.employeeUpdate],function(err, res) {
       if (err) throw err;
       console.table(res);
+      //return to main menu
       initialPrompt();
     });
   });
 }
-        
+//option to close the applicaion
 function exit() {
+  //shuts down the SQL instance
   db.end();
+  //ends node process
   process.exit();
   }
